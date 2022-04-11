a3f0b1f2be7d64bae916139bb29a24e0e868b40c
mosesmrima <mrimamss@gmail.com>
2022-04-11 23:16:43 +0300

    set indentation

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,11 +1,19 @@
 --setup the mysql dev env
 
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-USE hbnb_dev_db;
+CREATE USER
+       IF NOT EXISTS 'hbnb_dev'@'localhost'
+       IDENTIFIED BY 'hbnb_dev_pwd';
 
-GRANT ALL PRIVILEGES 0N hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+CREATE DATABASE
+       IF NOT EXISTS hbnb_dev_db;
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES
+      0N `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+
+GRANT SELECT
+      ON `performance_schema`.*
+      TO 'hbnb_dev'@'localhost'
+      IDENTIFIED BY 'hbnb_dev_pwd';
+
 FLUSH PRIVILEGES;
