3739566f708edb9240a8ec446ee7fd6d8577ce31
mosesmrima <mrimamss@gmail.com>
2022-04-11 15:48:15 +0300

    set mysql dev env

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
+
+GRANT ALL PRIVILEGES 0N hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
+
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
