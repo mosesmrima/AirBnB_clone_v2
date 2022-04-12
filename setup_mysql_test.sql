2f975ddae4f341c46036ab933194738bc31f8683
mosesmrima <mrimamss@gmail.com>
2022-04-12 14:31:26 +0300

    setup db test env

---
+++ setup_mysql_test.sql
@@ -0,0 +1,18 @@
+-- setup the db test env
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+CREATE USER
+       IF NOT EXISTS 'hbnb_test'@'localhost'
+       IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES
+      ON `hbnb_test_db`.*
+      TO hbnb_test@localhost
+      IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT SELECT
+      ON perfomance_schema.*
+       TO hbnb_test@localhost
+      IDENTIFIED BY 'hbnb_test_pwd';
+FLUSH PRIVILEGES;
