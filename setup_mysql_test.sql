a250e54ce90003662b82c0508d1cea5d32817fe2
mosesmrima <mrimamss@gmail.com>
2022-04-12 14:35:52 +0300

    refactor

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,18 +1,15 @@
 -- setup the db test env
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
 CREATE USER
-       IF NOT EXISTS 'hbnb_test'@'localhost'
-       IDENTIFIED BY 'hbnb_test_pwd';
-
+    IF NOT EXISTS 'hbnb_test'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
-      ON `hbnb_test_db`.*
-      TO hbnb_test@localhost
-      IDENTIFIED BY 'hbnb_test_pwd';
-
+   ON `hbnb_test_db`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
-      ON perfomance_schema.*
-       TO hbnb_test@localhost
-      IDENTIFIED BY 'hbnb_test_pwd';
+   ON `performance_schema`.*
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
