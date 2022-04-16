71611e6af7516ba244a9fdf32541ab4b65e6cb7f
mosesmrima <mrimamss@gmail.com>
2022-04-16 12:50:23 +0300

    orm

--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
+from models.city import City
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """State class"""
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+
+        @property
+        def cities(self):
+            """Get all cities related"""
+            c_list = []
+            for city in list(storage.all(City).values()):
+                if city.state_id == self.id:
+                    c_list.append(city)
+            return c_list
