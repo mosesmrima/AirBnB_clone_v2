6ea24aac0ed992b86b208ff6e211ae5357d098dc
mosesmrima <mrimamss@gmail.com>
2022-04-16 22:29:37 +0300

    fab file

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# compress
+
+import os.path
+from datetiem import datetime
+from fabric.api import local
+
+def do_pack():
+    """ Create a tar archive """
+    dt = datetime.utcnow()
+    path = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+        if local("tar -cvzf {} web_static".format(path)).failed is True:
+            return None
+        return path
